63e1075e31b44b0aeb17886e1b2462de9daa8909
Artur <arthur_bandaryk@mail.ru>
2023-02-26 16:06:19 +0300

    Render simple window

--- src/game.cpp
+++ src/game.cpp
@@ -1,5 +1,47 @@
 #include "game.hpp"
 
+#include <iostream>
+
+///////////////////////////////////////////////////////////////////////////////
+
 namespace arci {
 
+///////////////////////////////////////////////////////////////////////////////
+
+Game::Game() {
+  InitGame();
+  Run();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+void Game::InitGame() {
+  // Init window.
+  m_window.create(sf::VideoMode{800, 800}, "Tank2D", sf::Style::Close);
+  const auto desktop = sf::VideoMode::getDesktopMode();
+  m_window.setPosition(sf::Vector2i{
+      static_cast<int>(desktop.width / 2 - m_window.getSize().x / 2),
+      static_cast<int>(desktop.height / 2 - m_window.getSize().y / 2)});
+  m_window.setVerticalSyncEnabled(true);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+void Game::Run() {
+  while (m_window.isOpen()) {
+    sf::Event event;
+    while (m_window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed)
+        m_window.close();
+    }
+
+    m_window.clear();
+    m_window.display();
+  }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
 } // namespace arci
+
+///////////////////////////////////////////////////////////////////////////////
--- src/main.cpp
+++ src/main.cpp
@@ -3,25 +3,6 @@
 #include "game.hpp"
 
 int main(int /*argc*/, char** /*argv*/) {
-  // constexpr unsigned int kScreenWidth{800}, kScreenHeight{800};
-  // sf::RenderWindow window(
-  //     sf::VideoMode(kScreenWidth, kScreenHeight),
-  //     "Tank2D");
-
-  // sf::CircleShape shape(100.f);
-  // shape.setFillColor(sf::Color::Green);
-
-  // while (window.isOpen()) {
-  //   sf::Event event;
-  //   while (window.pollEvent(event)) {
-  //     if (event.type == sf::Event::Closed)
-  //       window.close();
-  //   }
-
-  //   window.clear();
-  //   window.draw(shape);
-  //   window.display();
-  // }
-  std::cout << arci::sum(1, 2) << std::endl;
+  arci::Game game{};
   return 0;
 }
